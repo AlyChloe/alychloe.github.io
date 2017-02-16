2b34e904df92bff3fac051a1d98561c470106acb
Chloe Collier <achloecollier@gmail.com>
2017-02-16 09:22:06 -0500

    added responsive css and loader icon

--- src/js/components/app-component.js
+++ src/js/components/app-component.js
@@ -25,6 +25,25 @@ angular.module('portfolioApp')
         {page:'contact', icon:'fa-envelope'}
       ];
       ctrl.social = ['twitter', 'instagram', 'github-alt'];
+
+      function onReady(callback) {
+          var intervalID = window.setInterval(checkReady, 1000);
+          function checkReady() {
+              if (document.getElementsByTagName('body')[0] !== undefined) {
+                  window.clearInterval(intervalID);
+                  callback.call(this);
+              }
+          }
+      }
+
+      function show(id, value) {
+          document.getElementById(id).style.display = value ? 'block' : 'none';
+      }
+
+      onReady(function () {
+          show('page', true);
+          show('loading', false);
+      });
     },
     bindings: {
       onViewChange: '&'
--- src/js/components/career-component.js
+++ src/js/components/career-component.js
@@ -5,15 +5,10 @@ angular.module('portfolioApp')
       let ctrl = this;
       let ref = appService.firebase;
 
-      ctrl.$onInit = function() {
-        console.log('Im working!');
-      }
-
       ref.once("value", function(snapshot) {
         console.log(snapshot.val());
         $scope.techSkills = snapshot.val().skills;
         $scope.career = snapshot.val().career;
-
         $scope.$apply();
       }, function (errorObject) {
         console.log("The read failed: " + errorObject.code);
