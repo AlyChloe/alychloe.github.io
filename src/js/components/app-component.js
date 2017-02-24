b826ee18ab12ac21ab02db0aedbf29f0551f8508
Chloe Collier <achloecollier@gmail.com>
2017-02-24 08:20:52 -0500

    removed loading spinner

--- src/js/components/app-component.js
+++ src/js/components/app-component.js
@@ -29,25 +29,6 @@ angular.module('portfolioApp')
         {media: 'instagram', url: 'https://www.instagram.com/achloecollier/'},
         {media: 'github-alt', url: 'https://github.com/AlyChloe'}
       ];
-
-      function onReady(callback) {
-          var intervalID = window.setInterval(checkReady, 1000);
-          function checkReady() {
-              if (document.getElementsByTagName('body')[0] !== undefined) {
-                  window.clearInterval(intervalID);
-                  callback.call(this);
-              }
-          }
-      }
-
-      function show(id, value) {
-          document.getElementById(id).style.display = value ? 'block' : 'none';
-      }
-
-      onReady(function () {
-          show('page', true);
-          show('loading', false);
-      });
     },
     bindings: {
       onViewChange: '&'
