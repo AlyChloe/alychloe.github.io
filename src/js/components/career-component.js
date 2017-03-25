429c42a049bce3546649074abd629bd339b4a1c8
Chloe Collier <achloecollier@gmail.com>
2017-03-25 15:30:48 -0400

    added ng-if

--- src/js/components/career-component.js
+++ src/js/components/career-component.js
@@ -6,7 +6,6 @@ angular.module('portfolioApp')
       let ref = appService.firebase;
 
       ref.once("value", function(snapshot) {
-        console.log(snapshot.val());
         $scope.techSkills = snapshot.val().skills;
         $scope.career = snapshot.val().career;
         $scope.$apply();
